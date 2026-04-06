54183b68a4c164b72a1bc8494c535ab6a86aae64
Jean-Jacques Martrès <jjmartrtes@gmail.com>
2026-04-06 15:57:10 +0200

    ✨ feat: disable spell checking in opencode terminal pane

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable spell checking in opencode terminal pane
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    local name = vim.api.nvim_buf_get_name(buf)
+    if name:find("opencode", 1, true) then
+      vim.opt_local.spell = false
+    end
+  end,
+  desc = "Disable spell in opencode terminal pane",
+})
